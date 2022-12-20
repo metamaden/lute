d9f59bb6c610bda5038c613082760d8a1cd925dd
Sean Maden <metamaden@users.noreply.github.com>
2022-12-20 10:05:17 -0800

    adds param descriptions

--- R/zref_utils.R
+++ R/zref_utils.R
@@ -8,13 +8,15 @@
 #' Get type data from a SingleCellExperiment, returning either an object of 
 #' either class SummarizedExperimentTypes or MultiAssayExperiment.
 #'
-#' @param sce
-#' @param top.markers
-#' @param bind.marker.results
-#' @param marker.method
-#' @param type.method
-#' @param return.type
-#' @param verbose
+#' @param sce Either a SummarizedExperiment or SingleCellExperiment object.
+#' @param num.top.markers Number of top markers to retain by type (e.g. total 
+#' markers is k*num.top.markers).
+#' @param bind.marker.results Whether to bind valid flat outputs from marker
+#' searches to the colData in the returned results object.
+#' @param marker.method Valid method to identify markers from sce object.
+#' @param type.method Valid method to obtain type-level expression/signals.
+#' @param return.type Class of results object to return.
+#' @param verbose Whether to show verbose status messages.
 #' @param ... Additional arguments to pass for marker discovery method.
 #' @returns Either a SummarizedExperimentTypes or or MultiAssayExperiment object.
 #' @export
