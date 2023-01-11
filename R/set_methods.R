b3341a3052ec38025a1b26bbb399f168960c1040
Sean Maden <metamaden@users.noreply.github.com>
2023-01-11 08:08:13 -0800

    adds set_from_set(), convert_sce()

--- R/set_methods.R
+++ R/set_methods.R
@@ -204,6 +204,106 @@ set_from_sce <- function(sce, groupvar = NULL, method = "mean",
   return(set)
 }
 
+#' set_from_set
+#'
+#' @param 
+#' @param 
+#' 
+#'
+set_from_set <- function(set, groupvar, method = "mean", typevar = "celltype", 
+                         assayname = "counts",  verbose = FALSE, ...){
+  if(!(is(sce, "SingleCellExperiment")|is(sce, "SummarizedExperiment"))){
+    stop("sce must be of class SingleCellExperiment or SummarizedExperiment.")}
+  typev <- unique(sce[[typevar]])
+  expr.sce <- assays(sce)$counts
+  if(!is(groupvar, "NULL")){
+    ugroupv <- unique(sce[[groupvar]]) # get all possible group lvls
+  }
+  expr.set <- do.call(cbind, lapply(typev, function(typei){
+    if(verbose){message("Summarizing type: ", typei, "...")}
+    scef <- sce[,sce[[typevar]]==typei]
+    exprf <- assays(scef)[[assayname]]
+    gene.varv <- apply(exprf,1,var)
+    gene.sdv <- apply(exprf,1,sd)
+    gene.max <- apply(exprf,1,max)
+    gene.min <- apply(exprf,1,min)
+    if(method == "mean"){
+      exprnew <- matrix(rowMeans(exprf), ncol = 1)
+    } else if(method == "median"){
+      exprnew <- matrix(rowMedians(exprf), ncol = 1)
+    } else{
+      exprnew <- exprf[,1,drop=F]
+    }
+    colnames(exprnew) <- paste0(typei, ";expr")
+    de <- data.frame(var = gene.varv, sdv = gene.sdv, max = gene.max,
+                     min = gene.min)
+    # parse group-level statistics
+    if(!is(groupvar, "NULL")){
+      dfg <- sce_groupstat(scef = scef, groupvar = groupvar, ugroupv = ugroupv,
+                           summarytype = "rowData", assayname = assayname, 
+                           verbose = verbose, ...)
+      condv <- is(dfg, "data.frame") & nrow(dfg) == nrow(de)
+      if(condv){if(verbose){message("Binding group-level data.")}
+        de <- cbind(de, dfg)}
+    }
+    colnames(de) <- paste0(typei, ";", colnames(de))
+    return(cbind(exprnew, de))
+  }))
+  which.mexpr <- grepl(".*;expr$", colnames(expr.set))
+  mexpr <- expr.set[,which.mexpr] # expr data
+  colnames(mexpr) <- gsub(";expr.*", "", colnames(mexpr))
+  rd <- expr.set[,!which.mexpr] # rowdata
+  
+  # get coldata
+  cd.sce <- colData(sce)
+  cd <- do.call(rbind, lapply(typev, function(typei){
+    if(verbose){message("Working on type: ", typei, "...")}
+    scef <- sce[,sce[[typevar]]==typei]
+    num.cells <- ncol(scef)
+    # parse 0-expr genes/cells
+    count.zeroexpr <- apply(assays(scef)$counts, 1, 
+                            function(ri){length(ri[ri==0])})
+    num.allzeroexpr <- length(which(count.zeroexpr==num.cells))
+    mean.zerocount <- mean(count.zeroexpr)
+    median.zerocount <- median(count.zeroexpr)
+    var.zerocount <- var(count.zeroexpr)
+    sd.zerocount <- sd(count.zeroexpr)
+    # get return df
+    dfr <- data.frame(type = typei,
+                      num.cells = num.cells,
+                      num.allzeroexpr = num.allzeroexpr,
+                      mean.zerocount = mean.zerocount,
+                      median.zerocount = median.zerocount,
+                      var.zerocount = var.zerocount,
+                      sd.zerocount = sd.zerocount)
+    # parse group-level statistics
+    if(!is(groupvar, "NULL")){
+      dfg <- sce_groupstat(scef = scef, groupvar = groupvar, ugroupv = ugroupv, 
+                           assayname = assayname, summarytype = "colData", 
+                           verbose = verbose, ...)
+      condv <- is(dfg, "data.frame") & nrow(dfg) == nrow(dfr)
+      if(condv){
+        if(verbose){message("Binding group-level data.")};dfr <- cbind(dfr, dfg)
+      }
+    }
+    return(dfr)
+  }))
+  
+  # make new set object
+  lassays <- list(mexpr); names(lassays) <- assayname
+  set <- SummarizedExperimentTypes(assays = lassays, rowData = rd, colData = cd)
+  return(set)
+}
+
+
+#' convert_sce
+#'
+#' Manage conversions to a `SummarizedExperimentTypes` object.
+#'
+#' @param
+#' @param 
+#' 
+#'
 
 #---------------------------
 # 3. methods for adjustments
