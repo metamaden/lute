216beca5ca2402b0604009ec969ef89f4b26013f
Sean Maden <metamaden@users.noreply.github.com>
2023-01-13 12:11:02 -0800

    updates hm examples

--- R/set_methods.R
+++ R/set_methods.R
@@ -458,16 +458,22 @@ get_set_plots <- function(){}
 #' @param verbose Whether to show verbose status messages.
 #' @results Returns heatmap object returned from ComplexHeatmap::Heatmap()
 #' @examples 
+#' # get randomized singlecellexperiment
 #' sce <- random_sce()
 #' sce[["donor"]] <- c(rep("donor1", 2), rep("donor2", 8))
 #' sce[["typevar"]] <- paste0(sce[["celltype"]], ";", sce[["donor"]])
 #' # make new set from sce
-#' set <- set_from_sce(sce, type.variable = "typevar")
-#' set[["donor"]] <- gsub(".*;", "", set[["type"]])
-#' set[["typevar"]] <- gsub(";.*", "", set[["type"]])
-#' rowData(set)$marker_type <- c(rep("type1", 10), rep("type2", 10))
-#' get_set_heatmap(set, type.variable = "typevar", group.variable = "donor",
-#'                 mtype.variable = "marker_type")
+#' set1 <- set_from_sce(sce, type.variable = "typevar")
+#' set1[["donor"]] <- gsub(".*;", "", set1[["type"]])
+#' set1[["typevar"]] <- gsub(";.*", "", set1[["type"]])
+#' rowData(set1)$marker_type <- c(rep("type1", 10), rep("type2", 10))
+#' # heatmap of type;donor assays
+#' hm1 <- get_set_heatmap(set1, type.variable = "typevar", 
+#'                        group.variable = "donor", mtype.variable = "marker_type")
+#' # heatmap of type assays
+#' set2 <- set_from_set(set1, typevar = "typevar")
+#' hm2 <- get_set_heatmap(set2, type.variable = "type", 
+#'                        mtype.variable = "marker_type")
 #' @export
 get_set_heatmap <- function(set, assayname = "logcounts_bytype",
                             type.variable = NULL, group.variable = NULL, 
--- inst/tests/test_set.R
+++ inst/tests/test_set.R
@@ -20,16 +20,15 @@ sce <- random_sce()
 sce[["donor"]] <- c(rep("donor1", 2), rep("donor2", 8))
 sce[["typevar"]] <- paste0(sce[["celltype"]], ";", sce[["donor"]])
 # make new set from sce
-set <- set_from_sce(sce, type.variable = "typevar")
-set[["donor"]] <- gsub(".*;", "", set[["type"]])
-set[["typevar"]] <- gsub(";.*", "", set[["type"]])
-rowData(set)$marker_type <- c(rep("type1", 10), rep("type2", 10))
-
+set1 <- set_from_sce(sce, type.variable = "typevar")
+set1[["donor"]] <- gsub(".*;", "", set1[["type"]])
+set1[["typevar"]] <- gsub(";.*", "", set1[["type"]])
+rowData(set1)$marker_type <- c(rep("type1", 10), rep("type2", 10))
 # heatmap of type;donor assays
-get_set_heatmap(set, type.variable = "typevar", group.variable = "donor",
-                mtype.variable = "marker_type")
-
+hm1 <- get_set_heatmap(set1, type.variable = "typevar", 
+                       group.variable = "donor", mtype.variable = "marker_type")
 # heatmap of type assays
-set2 <- set_from_set(set, typevar = "typevar")
-get_set_heatmap(set2, type.variable = "type", mtype.variable = "marker_type")
+set2 <- set_from_set(set1, typevar = "typevar")
+hm2 <- get_set_heatmap(set2, type.variable = "type", 
+                       mtype.variable = "marker_type")
 
