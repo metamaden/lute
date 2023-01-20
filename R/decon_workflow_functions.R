3e4237ee8af5d2499b7861fc2d4e550bf4a0e7fe
Sean Maden <metamaden@users.noreply.github.com>
2023-01-20 09:30:42 -0800

    rename na filt functions
    adds na filter functions
    adds filter on types
    renames function to filter on cells

--- R/decon_workflow_functions.R
+++ R/decon_workflow_functions.R
@@ -12,15 +12,17 @@
 # preprocessing -- filters
 #-------------------------
 
-#' filter_na_bytype
+#' filter_na_cells
 #'
-#' In some SingleCellExperiment, filter the NA frequency by type.
+#' Filter cells on NA frequency.
 #'
 #' @param sce A SingleCellExperiment object.
 #' @param type.variable Name of the type variable in sce colData.
 #' @param na.freq The upper threshold for tolerated NA frequency. Cells with NA 
 #' frequency exceeding this are excluded.
 #' @param assayname Name of assay in assays(sce) to check for NAs.
+#' @param append.metadata Whether to append metadata summary of preprocessing 
+#' parameters and results.
 #' @param verbose Whether to include verbose status messages.
 #' @returns scef, filtered SingleCellExperiment with optional appended 
 #' preprocessing metadata.
@@ -29,8 +31,9 @@
 #' scef <- filter_na_bytype(sce)
 #'
 #'
-filter_na_bytype <- function(sce, type.variable = "celltype", na.freq = 0.25, 
-                             assayname = "counts", verbose = FALSE){
+filter_na_cells <- function(sce, type.variable = "celltype", na.freq = 0.25,
+                          assayname = "counts", append.metadata = TRUE,
+                          verbose = FALSE){
   if(!is(sce, "SingleCellExperiment")){
     stop("Error, sce must be a SingleCellExperiment.")}
   cd <- colData(sce)
@@ -38,10 +41,27 @@ filter_na_bytype <- function(sce, type.variable = "celltype", na.freq = 0.25,
     stop("Error, type.variable must be a column name in sce colData.")
   }
   typev <- unique(cd[,typevar])
+  mexpr <- assays(sce)[[assayname]]
+  # count nas
+  na.freqv <- apply(mexpr, 2, function(ci){length(which(is.na(ci)))})
   
   
   
   # append metadata
+}
+
+#' filter_na_type
+#'
+#'
+filter_na_type <- function(){
+  
+}
+
+#' filter_na_sce
+#'
+#'
+filter_na_sce <- function(){
+  
 }
 
 #--------------
