7312d5ef86560c801a89b6501909b30ce71c5391
Sean Maden <metamaden@users.noreply.github.com>
2023-01-20 09:22:55 -0800

    adds filter_na_bytype()
    adds na bytype filter function

--- R/decon_workflow_functions.R
+++ R/decon_workflow_functions.R
@@ -12,10 +12,22 @@
 # preprocessing -- filters
 #-------------------------
 
+#' filter_na_bytype
+#'
+#' In some SingleCellExperiment, filter the NA frequency by type.
+#'
+#' @param sce A SingleCellExperiment object.
+#' @param type.variable Name of the type variable in sce colData.
+#' @param verbose Whether to include verbose status messages.
+#' @returns scef, filtered SingleCellExperiment
+#' @examples 
+#' sce <- random_sce(na.include = T, na.fract = 0.4)
+#' scef <- filter_na_bytype(sce)
 #'
 #'
-#'
-#'
+filter_na_bytype <- function(sce, type.variable = "celltype", verbose = FALSE){
+  
+}
 
 #--------------
 # normalization
