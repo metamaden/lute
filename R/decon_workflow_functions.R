3c2317e4312196683953034612df650b7bd02fc2
Sean Maden <metamaden@users.noreply.github.com>
2023-01-20 09:19:47 -0800

    Create decon_workflow_functions.R

---
+++ R/decon_workflow_functions.R
@@ -0,0 +1,26 @@
+#!/usr/bin/env R
+
+# Author: Sean Maden
+#
+# Defines functions used by NextFlow processes for lute workflow.
+#
+#
+#
+#
+
+#-------------------------
+# preprocessing -- filters
+#-------------------------
+
+#'
+#'
+#'
+#'
+
+#--------------
+# normalization
+#--------------
+
+#-----------------
+# bias adjustments
+#-----------------
