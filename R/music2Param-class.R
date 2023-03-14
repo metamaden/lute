6e0b5ab5fc6a082725d9a2ddc2730d7e49031b79
Sean Maden <metamaden@users.noreply.github.com>
2023-03-14 15:15:04 -0700

    Update music2Param-class.R
    begins adding slots for music2Param class

--- R/music2Param-class.R
+++ R/music2Param-class.R
@@ -1,17 +1,23 @@
 #' music2Param-class
 #' 
-#' Main constructor for class to manage mappings to the deconvolution
-#' method function \code{MuSiC::music2()}.
+#' Main constructor for class to manage mappings to the deconvolution method function \code{MuSiC::music2_prop()}.
 #' 
 #' @include lute_generics.R
 #' @include deconParam-class.R
 #' @include referencebasedParam-class.R
 #' @include independentbulkParam-class.R
+#' 
+#' @details Main constructor for class \linkS4class{music2Param}.
+#' @rdname music2Param-class
+#' @seealso \linkS4class{deconParam}, \linkS4class{referencebasedParam}, \linkS4class{independentbulkParam}
 #'
 #' @examples 
 #' lexample <- .get_decon_example_data()
 #' 
 #' @aliases 
-#' MuSiC2Param-class, Music2Param-class
+#' music2Param-class, MuSiC2Param-class, Music2Param-class
 #'
-setClass("music2Param", contains="independentbulkParam", slots=c(return.info = "logical"))
+setClass("music2Param", contains="independentbulkParam", slots=c(
+	sc.eset = "ExpressionSet", assay.name = "character", batch.variable = "character", 
+	celltype.variable = "character", iter.max = "numeric", nu = "numeric", epsilon = "numeric", 
+	truep = "numeric", ct.cell.size = "numeric", celltype.subset = "character"))
