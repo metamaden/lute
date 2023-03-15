3def364f2f9fb992cb3cc7832857c7c3faeb4e9a
Sean Maden <metamaden@users.noreply.github.com>
2023-03-15 09:54:31 -0700

    adds method_class column

--- inst/scripts/deconvolution_classes_diagram.R
+++ inst/scripts/deconvolution_classes_diagram.R
@@ -14,36 +14,84 @@ path <- system.file("csv", package = "lute")
 path <- file.path(path, csv.name)
 csv <- read.csv(path)
 
-#-------------------
-# get chart elements
-#-------------------
-# parse csv data
-# parse parent node edges
-input <- c("deconParam", "deconParam", 
-           "referencebasedParam", 
-           "referencebasedParam")
-output <- c("referencebasedParam",
-            "referencefreeParam",
-            "independentbulkParam",
-            "matchedbulkParam")
-# parse final child class edges
-paramv <- c("independentbulkParam", "matchedbulkParam")
-for(param in paramv){
-  filtv <- grepl(param, csv$parent_classes)
-  methodv <- csv[filtv,]$method_shortname
-  input <- c(input, rep(param, length(methodv)))
-  output <- c(output, paste0(tolower(methodv), "Param"))
-  nodev <- unique(c(input, output))
+#-----------
+# parameters
+#-----------
+append.string <- "Param"
+method.colname <- "method_shortname"
+parent.class.colname <- "parent_classes"
+
+#-----------------
+# helper functions
+#-----------------
+add_method_edges <- function(filter.string, csv, input, output,
+                             filter.type = "in", end.append.name = "Param",
+                             node.string = "referencebasedParam",
+                             filter.column = "parent_classes",
+                             method.column = "method_shortname"){
+  # adds edges for methods, where originating node is called node.string
+  method.vector <- csv[,method.column]
+  filter <- grepl(filter.string, method.vector)
+  if(!filter.type == "in"){
+    filter <- !grepl(filter.string, csv[,filter.column])}
+  # get formatted end node names/edges
+  new.edge.out <- tolower(unique(method.vector[filter]))
+  new.edge.out <- paste0(new.edge.out, end.append.name) # append string
+  new.edge.in <- rep(node.string, length(new.edge.out))
+  return(list(input = c(input, new.edge.in), 
+              output = c(output, new.edge.out)))
 }
 
-# get chart strings
-# get node strings
-node.string <- paste0("node [shape = box, ",
-                      "fontname = Helvectica]\n",
-                      paste0(nodev, collapse = ";"),
-                      "\n")
+#----------------
+# get chart edges
+#----------------
+# parse csv data
+# initialize parent class nodes and edges
+input <- c("deconParam", "deconParam", "referencebasedParam")
+output <- c("referencebasedParam", "referencefreeParam", "independentbulkParam")
+
+# parse final child class edges
+# append independent bulk methods
+lchart <- add_method_edges(filter.string = "independentbulkParam",
+                           filter.type = "in", csv = csv,
+                           input = input, output = output, 
+                           node.string = "independentbulkParam")
+# append remaining methods
+lchart <- add_method_edges(filter.string = "independentbulkParam", 
+                           filter.type = "out", csv = csv,
+                           lchart[["input"]], lchart[["output"]], 
+                           node.string = "referencebasedParam")
+
 # get edge strings
-edge.string <- paste0(input, "->", output, collapse = " ")
+edge.string <- paste0(lchart[["input"]], "->", 
+                      lchart[["output"]], collapse = " ")
+
+#----------------
+# get chart nodes
+#----------------
+# get chart strings
+methods <- paste0(unique(csv[,method.colname]), append.string)
+node.vector <- unique(c(lchart[["input"]], lchart[["output"]]))
+# get node strings
+filter <- node.vector %in% methods
+# set nodes for parent classes
+nodev.filter <- node.vector[!filter]
+node.string.parent <- paste0("node [shape = box, color = 'grey',",
+                      "fontname = Helvectica]\n",
+                      paste0(nodev.filter, collapse = ";"),
+                      "\n")
+# set nodes for methods
+nodev.filter <- node.vector[filter]
+node.string.method <- paste0("node [shape = cricle, fillcolor = grey, ",
+                      "fontname = Helvectica]\n",
+                      paste0(nodev.filter, collapse = ";"),
+                      "\n")
+# final node.string
+node.string <- paste0("\n\n",node.string.parent,"\n\n", node.string.method)
+
+#----------------------------------
+# make chart from component strings
+#----------------------------------
 # get chart strings
 chart.string <- paste0("digraph new_flowchart {\n",
                        "graph [overl = true, fonsize = 10]",
@@ -51,8 +99,4 @@ chart.string <- paste0("digraph new_flowchart {\n",
                        edge.string, "\n",
                        "}")
 
-#-----------
-# make chart
-#-----------
 grViz(chart.string)
-
