61783ba2e6670e68d0f0aae961b967a09a4fbc1a
Sean Maden <metamaden@users.noreply.github.com>
2023-03-16 09:46:32 -0700

    Update test_scdcParam-class.R
    clean up test script

--- inst/tests/test_scdcParam-class.R
+++ inst/tests/test_scdcParam-class.R
@@ -12,42 +12,6 @@ yi <- exprs(y.eset)[,filter]
 param <- scdcParam(yi = yi, y.eset = y.eset, sc.eset = sc.eset,
                    batch.variable = "SubjectName", 
                    celltype.variable = "cellType",
-                   celltype.subset = c("type1", "type2"))
+                   celltype.subset = NULL)
 # get just predictions
 result <- deconvolution(param)
-
-# get full results
-param@return.info <- TRUE
-result <- deconvolution(param)
-
-#-------------------
-# test scdc function
-#-------------------
-batch.variable <- "SubjectName"
-celltype.variable <- "cellType"
-
-lexample <- lute:::.get_decon_example_data_scdc()
-sc.eset <- lexample[["sc.eset"]]
-y.eset <- lexample[["y.eset"]]
-nu <- 1e-4
-epsilon <- 0.01
-iter.max <- 1000
-truep <- NULL
-weight.basis <- T
-unique.types <- unique(sc.eset[[celltype.variable]])
-celltype.subset <- unique.types[1:2]
-s <- rep(1, length(unique.types))
-names(s) <- unique.types
-
-result <- SCDC::SCDC_prop(bulk.eset = y.eset, 
-                          sc.eset = sc.eset,
-                          ct.varname = celltype.variable, 
-                          sample = batch.variable,
-                          iter.max = iter.max, 
-                          nu = nu, epsilon = epsilon, 
-                          truep = truep,
-                          ct.cell.size = s, 
-                          ct.sub = celltype.subset,
-                          Transform_bisque = F)
-
-
