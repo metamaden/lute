c3ab02b5bc7dbe2c642a7135c47b0925803e5de3
Sean Maden <metamaden@users.noreply.github.com>
2023-05-09 10:07:45 -0700

    Update lute_typemarker-selection.R
    updates docstrings for `markers_by_group`
    . adds args for parallelize, downsample

--- R/lute_typemarker-selection.R
+++ R/lute_typemarker-selection.R
@@ -43,6 +43,38 @@ get.group.markers.iter <- function(task.iter, sce, assay.name,
 }
 
 # this gets the markers by group, returning a list of markers by group
+
+#' markers_by_group
+#'
+#' Get gene markers organized by groups.
+#' 
+#' @param sce Object of type \linkS4class{SingleCellExperiment}.
+#' @param group.variable Variable name in \code{sce} coldata containing the 
+#' group ID labels.
+#' @param celltype.variable Variable name in \code{sce} coldata containing the 
+#' cell type labels.
+#' @param assay.name Name of the expression matrix in \code{sce} assays to use 
+#' for marker selection.
+#' @param markers.per.type Target maximum number of genes per cell type to select.
+#' @param typemarker.algorithm Algorithm for typemarker selection, supported in 
+#' \code{lute} (see \code{?typemarkers} for details).
+#' @param return.type Format of return data, either "list" or "table".
+#' @param downsample.within.group Whether to downsample on cell types prior to 
+#' marker selection (performed *within* groups)
+#' @param parallelize Whether to use parallelization (mclapply) when getting 
+#' markers by group. Otherwise uses lapply.
+#' @param verbose Whether to show verbose status messages.
+#' @returns List or table of markers (see \code{return.type} argument), 
+#' organized by group IDs.
+#' 
+#' @details Gets the within-group cell type markers.
+#' 
+#' @examples
+#' sce.example <- random_sce(num.cells = 100, num.genes = 100)
+#' sce.example[["sample_id"]] <- c(rep("sample1", 10), rep("sample2", 80), rep("sample1", 10))
+#' group.markers <- markers_by_group(sce.example)
+#' @seealso \code{filter_group_markers}
+#' @export
 markers_by_group <- function(sce, 
                              group.variable = "sample_id", 
                              celltype.variable = "celltype", 
@@ -280,70 +312,5 @@ marker_overlaps <- function(group.markers, marker.filter.vector = NULL){
   return(overlap.table)
 }
 
-#' markers_by_group
-#'
-#' Get gene markers organized by groups.
-#' 
-#' @param sce Object of type \linkS4class{SingleCellExperiment}.
-#' @param group.variable Variable name in \code{sce} coldata containing the 
-#' group ID labels.
-#' @param celltype.variable Variable name in \code{sce} coldata containing the 
-#' cell type labels.
-#' @param assay.name Name of the expression matrix in \code{sce} assays to use 
-#' for marker selection.
-#' @param markers.per.type Target maximum number of genes per cell type to select.
-#' @param typemarker.algorithm Algorithm for typemarker selection, supported in 
-#' \code{lute} (see \code{?typemarkers} for details).
-#' @param return.type Format of return data, either "list" or "table".
-#' @param verbose Whether to show verbose status messages.
-#' @returns List or table of markers (see \code{return.type} argument), 
-#' organized by group IDs.
-#' @details Gets the group markers for all individual specified groups, where
-#' "group" here means donor, batch, or some other set of cells over which we
-#' may be concerned about marker consistency.
-#' @examples
-#' sce.example <- random_sce(num.cells = 100, num.genes = 100)
-#' sce.example[["sample_id"]] <- c(rep("sample1", 10), rep("sample2", 80), rep("sample1", 10))
-#' group.markers <- markers_by_group(sce.example)
-#' @seealso \code{filter_group_markers}
-#' @export
-markers_by_group <- function(sce, 
-                             group.variable = "sample_id", 
-                             celltype.variable = "celltype", 
-                             assay.name = "counts", 
-                             markers.per.type = 20, 
-                             typemarker.algorithm = "meanratios",
-                             return.type = "list",
-                             verbose = FALSE){
-  require(dplyr)
-  unique.group.vector <- sce[[group.variable]] %>% unique() %>% as.character()
-  if(verbose){message("Getting gene markers for each specified batch")}
-  group.markers.list <- lapply(unique.group.vector, function(group.id){
-    if(verbose){message("Getting markers for batch id: ", group.id, "...")}
-    filter <- sce[[group.variable]] == group.id
-    sce.filter <- sce[,filter]
-    results <- meanratiosParam(sce = sce.filter, return.info = TRUE, 
-                               assay.name = assay.name,
-                               celltype.variable = celltype.variable, 
-                               markers.per.type = markers.per.type) %>% 
-      typemarkers()
-    result.table <- results$result.info
-    if(nrow(result.table) > 0){
-      result.table <- result.table[result.table$rank_ratio >= markers.per.type,]
-      topmarkers.vector <- results$markers
-      filter.topmarkers <- result.table$gene %in% topmarkers.vector
-      result.table <- result.table[filter.topmarkers,]
-      result.table$group.id <- group.id
-    }
-    return(result.table)
-  })
-  names(group.markers.list) <- unique.group.vector
-  if(return.type == "list"){
-    return.data <- group.markers.list
-    names(return.data)
-  } else{
-    return.data <- do.call(rbind, group.markers.list) %>% as.data.frame()
-  }
-  if(verbose){message("finished marker lists. Returning...")}
-  return(return.data)
-}
+
+
