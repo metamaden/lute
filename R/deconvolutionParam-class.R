489cee040da83e76e608fac75765d486b2cd898d
Sean Maden <metamaden@users.noreply.github.com>
2023-10-19 10:28:40 -0700

    Update deconvolutionParam-class.R

--- R/deconvolutionParam-class.R
+++ R/deconvolutionParam-class.R
@@ -30,11 +30,12 @@
 #' @returns New deconvolutionParam object.
 #'
 #' @export
-setClass("deconvolutionParam",  slots=c(y="matrix", return.info="logical"))
+setClass("deconvolutionParam",  
+         slots=c(bulkExpression="matrix", return.info="logical"))
 
 #' Inspect slot in \linkS4class{deconvolutionParam} object
-#' @param x Object to access.
-#' @param i Slot to access.
+#' @param objectToAccess Object to access.
+#' @param slotToAccess Slot to access.
 #' @returns Contents of specified slot.
 #' @details Inspect slot in \linkS4class{deconvolutionParam} object
 #' 
@@ -45,7 +46,8 @@ setClass("deconvolutionParam",  slots=c(y="matrix", return.info="logical"))
 #' @returns Object slot contents.
 #' 
 #' @export
-setMethod("[[", "deconvolutionParam", function(x, i) {slot(x, i)})
+setMethod("[[", "deconvolutionParam", function(objectToAccess, slotToAccess) {
+  slot(objectToAccess, slotToAccess)})
 
 #' Deconvolution generic behavior for object of class \linkS4class{deconvolutionParam}
 #' @param object An object of class \linkS4class{deconvolutionParam} (see 
@@ -73,15 +75,16 @@ setMethod("deconvolution", "deconvolutionParam", function(object) {})
 #' 
 #' @export
 setMethod("show", "deconvolutionParam", function(object) {
-  y <- object[["y"]]
+  bulkExpression <- object[["bulkExpression"]]
   message("Object of class deconvolutionParam")
   message("\nData summaries:")
-  message("\tNumber of bulk markers: ", nrow(y))
-  message("\tNumber of bulk samples: ", ncol(y))
-  markers <- rownames(y)
+  message("\tNumber of bulk markers: ", nrow(bulkExpression))
+  message("\tNumber of bulk samples: ", ncol(bulkExpression))
+  markers <- rownames(bulkExpression)
   if(length(markers) > 10){markers <- markers[seq(10)]}
-  message("\tFirst bulk marker labels:\n", paste0(rownames(y), collapse="; "))
-  samples <- colnames(y)
+  message("\tFirst bulk marker labels:\n", 
+          paste0(rownames(bulkExpression), collapse="; "))
+  samples <- colnames(bulkExpression)
   if(length(samples) > 10){samples <- samples[seq(10)]}
   message("\tFirst sample labels:\n", paste0(samples, collapse="; "), "\n\n")
 })
