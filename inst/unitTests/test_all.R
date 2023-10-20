4689506b89602e0083d185f9be81bf171e00b200
Sean Maden <metamaden@users.noreply.github.com>
2023-10-20 14:48:10 -0700

    adds test of standard deconvolution results, replaces expect_true with expect_equal where possible
    replaces expect_true as it is soft deprecated in testthat

--- inst/unitTests/test_all.R
+++ inst/unitTests/test_all.R
@@ -1,6 +1,6 @@
 test_that("randomSingleCellExperiment produces expected SingleCellExperiment", {
 
-	expect_true(is(randomSingleCellExperiment(), "SingleCellExperiment"))
+	expect_equal(class(randomSingleCellExperiment())[1], "SingleCellExperiment")
 	
 	expect_equal(names(assays(randomSingleCellExperiment())), "counts")
 
@@ -14,21 +14,37 @@ test_that("nnlsParam produces expected results", {
 	  bulkExpression=exampleList[["bulkExpression"]], 
 	  referenceExpression=exampleList[["referenceExpression"]]
 	)
-
-	expect_true(is(param, "nnlsParam"))
-
-	expect_true(is(param@referenceExpression, "matrix"))
-
-	expect_true(is(param@bulkExpression, "matrix"))
-
-	expect_true(is(param@cellScaleFactors, "numeric"))
-
-	expect_true(is(param, "referencebasedParam"))
-
-	expect_true(is(param, "deconvolutionParam"))
 	
-	expect_equal(names(assays(randomSingleCellExperiment())), "counts")
+	expect_equal(class(param)[1], "nnlsParam")
 
+	expect_equal(class(param@referenceExpression)[1], "matrix")
+
+	expect_equal(class(param@bulkExpression)[1], "matrix")
+
+	expect_equal(class(param@cellScaleFactors)[1], "numeric")
+
+	expect_equal(names(assays(randomSingleCellExperiment())), "counts")
+	
+	expect_true(inherits(param, c("referencebasedParam", "deconvolutionParam")))
+	
+})
+
+test_that("deconvolution() results have expected structure.", {
+  
+  exampleList <- getDeconvolutionExampleData()
+  param <- nnlsParam(
+    cellScaleFactors=exampleList[["cellScaleFactors"]], 
+    bulkExpression=exampleList[["bulkExpression"]], 
+    referenceExpression=exampleList[["referenceExpression"]]
+  )
+  deconvolutionResults <- deconvolution(param)
+  
+  expect_equal(class(deconvolutionResults)[1], "cellProportionsPredictions")
+  expect_equal(names(attributes(deconvolutionResults)[1]), "predictionsTable")
+  expect_equal(names(attributes(deconvolutionResults)[2]), "cellTypeVector")
+  expect_equal(names(attributes(deconvolutionResults)[3]), "sampleIdVector")
+  expect_equal(names(attributes(deconvolutionResults)[4]), "class")
+  
 })
 
 test_that("cellScaleFactors apply to the correct referenceExpression columns", {
@@ -39,6 +55,14 @@ test_that("cellScaleFactors apply to the correct referenceExpression columns", {
     bulkExpression=exampleList[["bulkExpression"]], 
     referenceExpression=exampleList[["referenceExpression"]]
   )
-  deconvolutionResults <- deconvolution(param)
+  
+  expect.vector.marker.1
+  
+  expect_true(identical())
+  
+  param@referenceExpression[,2]*param@cellScaleFactors[2]
   
 })
+
+
+
